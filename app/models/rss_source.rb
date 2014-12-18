9a8dc750e1169fd9874c82b224598b705b4f5942
xiongbo <xiongbo027@gmail.com>
2014-12-18 19:48:19 +0800

    add source model test

--- app/models/rss_source.rb
+++ app/models/rss_source.rb
@@ -1,2 +1,18 @@
 class RssSource < ActiveRecord::Base
+  validates :url, presence: true
+
+
+  private
+  before_create :set_title
+  def set_title
+    if rss = get_rss(url)
+      feed = Feedjira::Feed.fetch_and_parse(rss)
+    end
+    self.title = feed.try(:title) || ""
+  end
+
+  def get_rss(url)
+    rss = Feedbag.find(url) 
+    if rss.empty? then nil else rss[0] end
+  end
 end
--- test/models/rss_source_test.rb
+++ test/models/rss_source_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
-class RssSourceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class RssSourceTest < ActiveSupport::TestCase 
+  fixtures :rss_sources
+  test "source presence" do 
+    source = RssSource.new
+    source.save
+    assert source.errors.keys.include?(:url)
+  end
+
+  test "auto generate title for source" do 
+    source = rss_sources(:xiongbo) 
+    new_source = RssSource.new(url: source.url)
+    new_source.save
+
+    assert_equal source.title, new_source.title
+  end
 end
