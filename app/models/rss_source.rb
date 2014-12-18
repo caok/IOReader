cd4486accf440209a840b3d77c25718a4a920b50
xiongbo <xiongbo027@gmail.com>
2014-12-18 22:12:55 +0800

    update source model tests

--- app/models/rss_source.rb
+++ app/models/rss_source.rb
@@ -1,18 +1,25 @@
 class RssSource < ActiveRecord::Base
   validates :url, presence: true
-
+  validate :url_should_valid 
 
   private
-  before_create :set_title
+  before_save :set_title
   def set_title
-    if rss = get_rss(url)
+    return unless errors.empty?
+    if rss = get_rss
       feed = Feedjira::Feed.fetch_and_parse(rss)
     end
     self.title = feed.try(:title) || ""
   end
 
-  def get_rss(url)
-    rss = Feedbag.find(url) 
-    if rss.empty? then nil else rss[0] end
+  def url_should_valid
+    if url.present? and get_rss.nil?
+      errors.add(:url, "#{url} is not a valid rss feed.")
+    end
   end
+
+  def get_rss
+    rss = Feedbag.find(url)
+    if rss.empty? then nil else rss[0] end
+  end 
 end
--- test/models/rss_source_test.rb
+++ test/models/rss_source_test.rb
@@ -2,17 +2,38 @@ require 'test_helper'
 
 class RssSourceTest < ActiveSupport::TestCase 
   fixtures :rss_sources
+
   test "source presence" do 
     source = RssSource.new
     source.save
     assert source.errors.keys.include?(:url)
   end
 
-  test "auto generate title for source" do 
+  test "invalid rss source" do 
+    invalid_url = "www.google.com"
+    invalid_source = RssSource.new(url: invalid_url)
+    invalid_source.save 
+
+    assert invalid_source.errors.keys.include?(:url)
+  end
+
+  test "auto generate title for valid rss source" do 
     source = rss_sources(:xiongbo) 
     new_source = RssSource.new(url: source.url)
     new_source.save
 
     assert_equal source.title, new_source.title
+  end
+
+  test "get_rss with valid url" do 
+    source = rss_sources(:xiongbo)
+    assert source.send(:get_rss)
+  end 
+
+  test "get_rss with invalid url" do
+    invalid_url = "www.google.com"
+    invalid_source = RssSource.new(url: invalid_url)
+
+    assert_nil invalid_source.send(:get_rss)
   end
 end
