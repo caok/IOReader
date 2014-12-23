6572cade0e1aaea289bb0a5da21e9e3d279691b6
xiongbo <xiongbo027@gmail.com>
2014-12-23 23:06:46 +0800

    add source index and create action

--- app/controllers/rss_sources_controller.rb
+++ app/controllers/rss_sources_controller.rb
@@ -1,6 +1,26 @@
-class RssSourcesController < ApplicationController
+class RssSourcesController < ApplicationController 
+  before_action :authorize
+  before_action :set_source, only: [:edit, :update]
 
-  def new
-    
+  def index 
+    @rss_sources = current_user.rss_sources
+  end
+
+  def create 
+    if @source = current_user.rss_sources.create(source_params)
+      render json: @source
+    end
+  end
+
+  private
+  def source_params
+    params.require(:rss_source).permit(
+      :url, :user, :title)
+  end
+
+  def set_source
+    if params[:id]
+      @source = RssSource.find(:id)
+    end
   end
 end
